5d681dc9cf5cae1a8a1d86c3a9167b78d9bdb082
yooja <wogud758787@naver.com>
2021-11-05 00:52:28 +0900

    route 10.5

--- src/app.jsx
+++ src/app.jsx
@@ -1,13 +1,25 @@
+import {BrowserRouter, Route, Switch } from 'react-router-dom'
 import React from 'react';
 import styles from './app.module.css';
 import Login from './components/login/login';
+import Maker from './components/maker/maker';
 
 function App({ authService }) {
-  return (
-    <div className={styles.app}>
-      <Login authService={authService} />
-    </div>
-  );
+	return (
+		<div className={styles.app}>
+			<BrowserRouter>
+				<Switch>
+					<Route exact path="/">
+						<Login authService={authService} />
+					</Route>
+					<Route path="/maker">
+						<Maker authService={authService}/>
+					</Route>
+				</Switch>
+			</BrowserRouter>
+
+		</div>
+	);
 }
 
 export default App;
--- src/components/login/login.jsx
+++ src/components/login/login.jsx
@@ -1,35 +1,51 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
 import Footer from '../footer/footer';
 import Header from '../header/header';
 import styles from './login.module.css';
 
 const Login = ({ authService }) => {
-  const onLogin = event => {
-    authService //
-      .login(event.currentTarget.textContent)
-      .then(console.log);
-  };
-  return (
-    <section className={styles.login}>
-      <Header />
-      <section>
-        <h1>Login</h1>
-        <ul className={styles.list}>
-          <li className={styles.item}>
-            <button className={styles.button} onClick={onLogin}>
-              Google
-            </button>
-          </li>
-          <li className={styles.item}>
-            <button className={styles.button} onClick={onLogin}>
-              Github
-            </button>
-          </li>
-        </ul>
-      </section>
-      <Footer />
-    </section>
-  );
+	const history = useHistory();
+	const goToMaker = (userId) => {
+		history.push({
+			pathname: '/maker',
+			state: {id:userId},
+		});
+	}
+
+	useEffect(()=>{
+		authService
+			.onAuthChange(user => {
+				user && goToMaker(user.uid);
+			})
+	})
+
+	const onLogin = event => {
+		authService //
+		.login(event.currentTarget.textContent)
+		.then(data => goToMaker(data.user.uid));
+	};
+	return (
+		<section className={styles.login}>
+		<Header />
+		<section>
+			<h1>Login</h1>
+			<ul className={styles.list}>
+			<li className={styles.item}>
+				<button className={styles.button} onClick={onLogin}>
+				Google
+				</button>
+			</li>
+			<li className={styles.item}>
+				<button className={styles.button} onClick={onLogin}>
+				Github
+				</button>
+			</li>
+			</ul>
+		</section>
+		<Footer />
+		</section>
+	);
 };
 
 export default Login;
