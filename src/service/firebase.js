ee0658b4da46a5a2c64013acffc05eed926d5e38
yooja <wogud758787@naver.com>
2021-11-04 17:43:32 +0900

    login

---
+++ src/service/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+	apiKey: process.env.REACT_APP_API_KEY,
+	authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+	databaseURL: process.env.REACT_APP_DB_URL,
+	projectId: process.env.REACT_APP_PROJECT_ID,
+};
+
+firebaseConfig.initializeApp(firebaseConfig);
