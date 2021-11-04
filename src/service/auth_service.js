58fa51c16119cd3e0866fda51662b38af6255e0d
yooja <wogud758787@naver.com>
2021-11-04 22:52:36 +0900

    login UI 10.2

--- src/service/auth_service.js
+++ src/service/auth_service.js
@@ -1,10 +1,11 @@
 import firebase from 'firebase';
+import firebaseApp from './firebase';
 
 class AuthService {
-	login(providerName) {
-		const authProvider = new firebase.auth[`${providerName}AuthProvider`]();
-		return firebase.auth().signInWithPopup(authProvider);
-	}
+  login(providerName) {
+    const authProvider = new firebase.auth[`${providerName}AuthProvider`]();
+    return firebaseApp.auth().signInWithPopup(authProvider);
+  }
 }
 
 export default AuthService;
