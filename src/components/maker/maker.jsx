05cf2ebfcd497287b227564c955c3ac753bfe133
yooja <wogud758787@naver.com>
2021-11-05 16:20:42 +0900

    10.11

--- src/components/maker/maker.jsx
+++ src/components/maker/maker.jsx
@@ -1,33 +1,73 @@
-import React, { useEffect } from 'react';
-import styles from './maker.module.css';
-import Header from '../header/header';
-import Footer from '../footer/footer';
+import React, { useEffect, useState } from 'react';
 import { useHistory } from 'react-router-dom';
+import Footer from '../footer/footer';
+import Header from '../header/header';
 import Editor from '../editor/editor';
 import Preview from '../preview/preview';
+import styles from './maker.module.css';
 
-const Maker = ({authService}) => {
-	const history = useHistory();
-	const onLogout = () => {
-		authService.logout();
-	}
-	useEffect(() => {
-		authService.onAuthChange(user => {
-		  if (!user) {
-			history.push('/');
-		  }
-		});
-	  });
-	return (
-		<section className={styles.maker}>
-			<Header onLogout={onLogout}/>
-			<div className={styles.container}>
-				<Editor />
-				<Preview />
-			</div>
-			<Footer />
-		</section>
-	)
-}
+const Maker = ({ authService }) => {
+  const [cards, setCards] = useState([
+    {
+      id: '1',
+      name: 'Ellie',
+      company: 'Samsung',
+      theme: 'dark',
+      title: 'Software Engineer',
+      email: 'ellie@gmail.com',
+      message: 'go for it',
+      fileName: 'ellie',
+      fileURL: null,
+    },
+    {
+      id: '2',
+      name: 'Ellie2',
+      company: 'Samsung',
+      theme: 'light',
+      title: 'Software Engineer',
+      email: 'ellie@gmail.com',
+      message: 'go for it',
+      fileName: 'ellie',
+      fileURL: 'ellie.png',
+    },
+    {
+      id: '3',
+      name: 'Ellie3',
+      company: 'Samsung',
+      theme: 'colorful',
+      title: 'Software Engineer',
+      email: 'ellie@gmail.com',
+      message: 'go for it',
+      fileName: 'ellie',
+      fileURL: null,
+    },
+  ]);
+  const history = useHistory();
+  const onLogout = () => {
+    authService.logout();
+  };
+
+  useEffect(() => {
+    authService.onAuthChange(user => {
+      if (!user) {
+        history.push('/');
+      }
+    });
+  });
+
+  const addCard = card => {
+    console.log(card);
+  };
+  return (
+    <section className={styles.maker}>
+      <Header onLogout={onLogout} />
+      <div className={styles.container}>
+        <Editor cards={cards} addCard={addCard} />
+        <Preview cards={cards} />
+      </div>
+      <Footer />
+    </section>
+  );
+};
 
 export default Maker;
